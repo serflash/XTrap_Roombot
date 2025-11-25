cbcf7c4ba25229f2fab743ed684e6c9f7d590590
serflash <44881861+serflash@users.noreply.github.com>
2025-11-25 15:10:06 +0300

    Update t.js

--- src/utils/t.js
+++ src/utils/t.js
@@ -1,6 +1,6 @@
 import lang from '../../language/language.js'
 
-export default (key, language = 'en', values = {}) => {
+export default (key, language = 'ru', values = {}) => {
   let str = lang[language]?.[key] || lang.en?.[key] || key
   for (const [k, v] of Object.entries(values)) {
     str = str.replace(`{${k}}`, v)
